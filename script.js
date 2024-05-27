5e9a845f93bcf9748c20aa0576a66fd0467a896d
naval7871 <53475989+naval7871@users.noreply.github.com>
2024-05-27 19:00:56 +0530

    added terminate worker button

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const taskBtn = document.getElementById('taskBtn');
 const taskList = document.getElementById('taskList');
 const expensiveResult = document.getElementById('expensiveResult');
+const expensiveTaskBtn = document.getElementById('expensiveTask');
 
-const myWorker = new Worker("worker.js");
+const terminateBtn = document.getElementById('terminateTask');
+let myWorker;
+
+function webWorkerCreation(){
+    myWorker = new Worker("worker.js");
+}
 
 taskBtn.addEventListener('click', () => {
     let li = document.createElement('li');
@@ -11,22 +17,25 @@ taskBtn.addEventListener('click', () => {
 })
 
 
-const expensiveTaskBtn = document.getElementById('expensiveTask');
 expensiveTaskBtn.addEventListener('click', expensiveOperation)
+terminateBtn.addEventListener('click', terminateWebWorker)
+
+
 
 function expensiveOperation() {
     console.log('loading')
 
-    // without web worker - running expensive operation in the main thread --- START
-    // let result = expensiveFun()
-    // expensiveResult.innerText = result;
-    // without web worker - running expensive operation in the main thread --- END
+    // without web worker - running expensive operation in the main thread.
+        // let result = expensiveFun()
+        // expensiveResult.innerText = result;
+    // without web worker - running expensive operation in the main thread.
 
 // -------------------------------------------------------------------------------------------
 
-    // with Web worker --- START
+    // with Web worker
+        webWorkerCreation()
         callWebWoker();
-    // with Web worker --- END
+    // with Web worker
 }
 
 function expensiveFun() {
@@ -43,4 +52,9 @@ function callWebWoker(){
         console.log('Received message from worker:', event.data);
         expensiveResult.innerText = event.data;
     };
+}
+
+function terminateWebWorker(){
+    myWorker.terminate();
+    console.log('web worker is terminated')
 }
